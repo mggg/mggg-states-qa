a42977920ef3bb83d488dd68ddd33d5917cf92cf
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2020-06-24 18:00:29 -0400

    moved tests to different file

--- geodata/ExtractTable.py
+++ geodata/ExtractTable.py
@@ -330,6 +330,7 @@ examples:
     return parser.parse_args()
 
 
+
 #########################################
 # Main                                  #
 #########################################
@@ -344,158 +345,10 @@ def main() -> NoReturn:
     sys.exit()
 
 
-#########################################
-# Regression Tests                      #
-#########################################
-
-def run_tests():
-    print('et = ExtractTable()')
-    et = ExtractTable()
-    print('infile = ', et.infile)
-    print('outfile = ', et.outfile)
-    print('column = ', et.column)
-    print('value = ', et.value)
-    print()
-
-    try:
-        et.value = 'fail'
-    except Exception as e:
-        print('Expected failure:', e)
-    
-    try:
-        et.column = 'fail'
-    except Exception as e:
-        print('Expected failure:', e)
-
-    try:
-        et.infile = "test/asdf"
-    except Exception as e:
-        print('Expected failure.', e)
-    
-    try:
-        extracted = et.extract()
-    except Exception as e:
-        print('Expected failure.', e)
-    print()
-
-    et.infile = "test/test1.csv"
-
-    try:
-        et.value = "sdf"
-    except Exception as e:
-        print('Expected failure.', e)
-    
-    try:
-        et.infile = "asdf/asdf"
-    except Exception as e:
-        print('Expected failure.', e)
-    
-    try:
-        et.column = "col"
-    except Exception as e:
-        print('Expected failure.', e)
-
-    print(et.extract())
-    print()
-
-    et.column = "col1"
-    
-    try:
-        et.value = "fda"
-    except Exception as e:
-        print('Expected failure.', e)
-    
-    try:
-        et.column = "col"
-    except Exception as e:
-        print('Expected failure.', e)
-    
-    try:
-        et.value = "fda"
-    except Exception as e:
-        print('Expected failure.', e)
-
-    print(et.extract())
-    print()
-
-    et.value = "c"
-    print(et.extract())
-    print()
-
-
-def run_tests2():
-    try:
-        et = ExtractTable.read_file()
-    except Exception as e:
-        print('Expected failure.', e)
-    
-    try:
-        et = ExtractTable.read_file("asdf/asdf")
-    except Exception as e:
-        print('Expected failure.', e)
-
-    try:
-        et = ExtractTable.read_file(column=2, value='asdf')
-    except Exception as e:
-        print('Expected failure.', e)
-    
-    try:
-        et = ExtractTable.read_file("test/test1.csv", column="3")
-    except Exception as e:
-        print('Expected failure.', e) 
-
-    try:
-        et = ExtractTable.read_file("test/test1.csv", value="3")
-    except Exception as e:
-        print('Expected failure.', e) 
-
-    try:
-        et = ExtractTable.read_file("test/test1.csv", column="col1", value=3)
-    except Exception as e:
-        print('Expected failure.', e) 
-    print()
-
-    et = ExtractTable.read_file("test/test1.csv")
-    print('infile = ', et.infile)
-    print('outfile = ', et.outfile)
-    print('column = ', et.column)
-    print('value = ', et.value)
-    print(et.extract())
-    print()
-
-
-    et = ExtractTable.read_file("test/test1.csv", column="col1")
-    print('infile = ', et.infile)
-    print('outfile = ', et.outfile)
-    print('column = ', et.column)
-    print('value = ', et.value)
-    print(et.extract())
-    print()
-
-    et = ExtractTable.read_file("test/test1.csv", column="col1", value="c")
-    print('infile = ', et.infile)
-    print('outfile = ', et.outfile)
-    print('column = ', et.column)
-    print('value = ', et.value)
-    print(et.extract())
-    print() 
-
-    et = ExtractTable.read_file("test/test1.csv", column="col1", value=['a', 'c'])
-    print('infile = ', et.infile)
-    print('outfile = ', et.outfile)
-    print('column = ', et.column)
-    print('value = ', et.value)
-    print(et.extract())
-    print()
-
 
 #########################################
 # Function Calls                        #
 #########################################
 if __name__ == "__main__":
-        #main()
-        #run_tests()
-        run_tests2()
-
-
+    main()
 
