89fec75027e7cf97a05b6eeaad91d9b81eb0ac4f
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2020-07-01 19:47:00 -0400

    updated path to rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../../extract-table'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
