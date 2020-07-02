842c0d9ad0e1f0b369384496758ffa96db88b465
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2020-07-01 20:00:49 -0400

    added sphinx rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -36,7 +37,14 @@ master_doc = 'index' # RTD expects 'contents.rst' as main src instead
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = [
+        'sphinx.ext.autodoc', 
+        'sphinx.ext.coverage', 
+        'sphinx.ext.napoleon',
+        'sphinx_rtd_theme'
+]
+
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
