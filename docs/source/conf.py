5da29349826182a5b9621a7e37759e25993854fc
Keifer Chiang <15503711+KeiferC@users.noreply.github.com>
2020-07-01 20:35:23 -0400

    added rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,6 @@ extensions = [
         'sphinx_rtd_theme',
 ]
 
-html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -60,7 +59,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
